75e8413bf625f7cf0901edc04a1344cea88133b3
Max Korolev <37272192+Ladsers@users.noreply.github.com>
2021-02-28 16:04:50 +0300

    Implemented a simple way to open file.

--- app/src/main/java/com/ladsers/passtable/android/MainActivity.kt
+++ app/src/main/java/com/ladsers/passtable/android/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.ladsers.passtable.android
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ladsers.passtable.android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnOpenFile.setOnClickListener { openFile() }
+    }
+
+    private fun openFile() {
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
+        intent.addCategory(Intent.CATEGORY_OPENABLE)
+        intent.type = "application/*" //TODO: try to catch only passtable files.
+        startActivityForResult(intent, 1)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode != RESULT_OK) return
+        if (data == null) return
+
+        val uri = data.data ?: return
+        /*val inputStream = contentResolver.openInputStream(uri)
+        val cryptData = BufferedReader(InputStreamReader(inputStream)).readText()*/
+
+        val perms = Intent.FLAG_GRANT_READ_URI_PERMISSION or Intent.FLAG_GRANT_WRITE_URI_PERMISSION
+        contentResolver.takePersistableUriPermission(uri, perms)
     }
 }
