1d8011d20d5cc51d649f3e3c29a867a649108fb0
Max Korolev <37272192+Ladsers@users.noreply.github.com>
2021-03-19 10:34:52 +0300

    Refactoring: combining opening and creation methods.

--- app/src/main/java/com/ladsers/passtable/android/MainActivity.kt
+++ app/src/main/java/com/ladsers/passtable/android/MainActivity.kt
@@ -14,6 +14,8 @@ import com.ladsers.passtable.android.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
+    private var newFile: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -23,8 +25,8 @@ class MainActivity : AppCompatActivity() {
         //binding.toolbar.root.navigationIcon = //TODO: passtable logo
         setSupportActionBar(binding.toolbar.root)
 
-        binding.btnOpenFile.setOnClickListener { openFile() }
-        binding.btNewFile.setOnClickListener { newFile() }
+        binding.btnOpenFile.setOnClickListener { fileWorker(false) }
+        binding.btNewFile.setOnClickListener { fileWorker(true) }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -42,54 +44,37 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun newFile(){
-        val intent = Intent(Intent.ACTION_CREATE_DOCUMENT)
+    private fun fileWorker(newFile: Boolean){
+        this.newFile = newFile
+
+        val intent = if (newFile) Intent(Intent.ACTION_CREATE_DOCUMENT)
+        else Intent(Intent.ACTION_OPEN_DOCUMENT)
         intent.addCategory(Intent.CATEGORY_OPENABLE)
         intent.type = "application/*" //TODO: try to catch only passtable files.
-        newFileActivityResult.launch(intent)
+        fileActivityResult.launch(intent)
     }
 
-    private val newFileActivityResult = registerForActivityResult(
+    private val fileActivityResult = registerForActivityResult(
         ActivityResultContracts
             .StartActivityForResult()
     ) { result ->
         if (result.resultCode != Activity.RESULT_OK) return@registerForActivityResult
 
         var uri = result.data?.data ?: return@registerForActivityResult //TODO: err msg
-
-        val originalName = getFileNameWithExt(uri) ?: return@registerForActivityResult //TODO: err msg
-        if (!originalName.endsWith(".passtable")){
-            val saveName = "$originalName.passtable"
-            uri = DocumentsContract.renameDocument(contentResolver, uri, saveName)!!
+        if (newFile) {
+            val originalName =
+                getFileNameWithExt(uri) ?: return@registerForActivityResult //TODO: err msg
+            if (!originalName.endsWith(".passtable")) {
+                val saveName = "$originalName.passtable"
+                uri = DocumentsContract.renameDocument(contentResolver, uri, saveName)!!
+            }
         }
         val perms = Intent.FLAG_GRANT_READ_URI_PERMISSION or Intent.FLAG_GRANT_WRITE_URI_PERMISSION
         contentResolver.takePersistableUriPermission(uri, perms)
 
         val intent = Intent(this, TableActivity::class.java)
         intent.putExtra("fileUri", uri)
-        intent.putExtra("newFile", true)
-        startActivity(intent)
-    }
-
-    private fun openFile() {
-        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
-        intent.addCategory(Intent.CATEGORY_OPENABLE)
-        intent.type = "application/*" //TODO: try to catch only passtable files.
-        openFileActivityResult.launch(intent)
-    }
-
-    private val openFileActivityResult = registerForActivityResult(
-        ActivityResultContracts
-            .StartActivityForResult()
-    ) { result ->
-        if (result.resultCode != Activity.RESULT_OK) return@registerForActivityResult
-
-        val uri = result.data?.data ?: return@registerForActivityResult //TODO: err msg
-        val perms = Intent.FLAG_GRANT_READ_URI_PERMISSION or Intent.FLAG_GRANT_WRITE_URI_PERMISSION
-        contentResolver.takePersistableUriPermission(uri, perms)
-
-        val intent = Intent(this, TableActivity::class.java)
-        intent.putExtra("fileUri", uri)
+        intent.putExtra("newFile", newFile)
         startActivity(intent)
     }
 }
