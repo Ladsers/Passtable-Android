bf2044c5669b8c07385947d9f186ef541082f1df
Max Korolev <37272192+Ladsers@users.noreply.github.com>
2021-04-13 17:00:52 +0300

    Implemented the ability to store master passwords in encrypted form in object "RecentFiles".

--- app/src/main/java/com/ladsers/passtable/android/RecentFiles.kt
+++ app/src/main/java/com/ladsers/passtable/android/RecentFiles.kt
@@ -31,27 +31,40 @@ object RecentFiles {
             strDate.split("|").toMutableList()
         } else mutableListOf()
 
+        val strMpEncrypted = shPref.getString("mpEncrypted", "")
+        val mpEncryptedList = if (!strMpEncrypted.isNullOrEmpty()) {
+            strMpEncrypted.split("|").toMutableList()
+        } else mutableListOf()
+
+        var currentMpEncrypted = " "
         val index = uriList.indexOf(data)
         if (index != -1) {
             uriList.removeAt(index)
             dateList.removeAt(index)
+
+            currentMpEncrypted = mpEncryptedList[index]
+            mpEncryptedList.removeAt(index)
         }
 
-        val maxItems = shPref.getInt("maxItems", 5)
         if (isAdding) {
             uriList.add(data)
             dateList.add(getCurrentDateAsStr())
+            mpEncryptedList.add(currentMpEncrypted)
+            val maxItems = shPref.getInt("maxItems", 5)
             if (uriList.size > maxItems) {
                 uriList.removeAt(0)
                 dateList.removeAt(0)
+                mpEncryptedList.removeAt(0)
             }
         }
 
         val newStrUri = uriList.joinToString("|") { it.toString() }
         val newStrDate = dateList.joinToString("|")
+        val newStrMpEncrypted = mpEncryptedList.joinToString("|")
         with(shPref.edit()) {
             putString("uri", newStrUri)
             putString("date", newStrDate)
+            putString("mpEncrypted", newStrMpEncrypted)
             apply()
         }
         return true
@@ -77,6 +90,14 @@ object RecentFiles {
         return strList.reversed().toMutableList()
     }
 
+    fun getLastMpEncrypted(context: Context?): String? {
+        val shPref = context?.getSharedPreferences("recentFiles", Context.MODE_PRIVATE)
+            ?: return null
+        val strMpEncrypted = shPref.getString("mpEncrypted", "")
+        val mpEncryptedList = strMpEncrypted!!.split("|").toMutableList()
+        return mpEncryptedList[mpEncryptedList.lastIndex]
+    }
+
     fun clear(context: Context?): Boolean {
         val shPref = context?.getSharedPreferences("recentFiles", Context.MODE_PRIVATE)
             ?: return false
@@ -84,8 +105,38 @@ object RecentFiles {
         with(shPref.edit()) {
             putString("uri", "")
             putString("date", "")
+            putString("mpEncrypted", "")
             apply()
         }
+        return true
+    }
+
+    fun forgetMpsEncrypted(context: Context?): Boolean {
+        val shPref = context?.getSharedPreferences("recentFiles", Context.MODE_PRIVATE)
+            ?: return false
+
+        val strUri = shPref.getString("uri", "")
+        val uriList = if (!strUri.isNullOrEmpty()) {
+            val strList = strUri.split("|")
+            strList.map { it.toUri() }.toMutableList()
+        } else mutableListOf()
+
+        val mpEncryptedList = mutableListOf<String>()
+        while (mpEncryptedList.size < uriList.size) mpEncryptedList.add(" ")
+        val newStrMpEncrypted = mpEncryptedList.joinToString("|")
+        shPref.edit().putString("mpEncrypted", newStrMpEncrypted).apply()
+        return true
+    }
+
+    fun rememberLastMpEncrypted(context: Context?, data: String): Boolean {
+        val shPref = context?.getSharedPreferences("recentFiles", Context.MODE_PRIVATE)
+            ?: return false
+        val strMpEncrypted = shPref.getString("mpEncrypted", "")
+        val mpEncryptedList = strMpEncrypted!!.split("|").toMutableList()
+        mpEncryptedList[mpEncryptedList.lastIndex] = data
+        val newStrMpEncrypted = mpEncryptedList.joinToString("|")
+
+        shPref.edit().putString("mpEncrypted", newStrMpEncrypted).apply()
         return true
     }
 
