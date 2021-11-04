66c64dedda1d9c1f53fbf9a41586cd55d3431173
Max Korolev <37272192+Ladsers@users.noreply.github.com>
2021-11-04 16:53:43 +0300

    Improved logic and design of the primary password request window.

--- app/src/main/java/com/ladsers/passtable/android/MpRequester.kt
+++ app/src/main/java/com/ladsers/passtable/android/MpRequester.kt
@@ -1,5 +1,6 @@
 package com.ladsers.passtable.android
 
+import Verifier
 import android.app.Activity
 import android.content.ContentResolver
 import android.content.Context
@@ -7,15 +8,19 @@ import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.net.Uri
 import android.provider.DocumentsContract
-import android.text.Editable
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
 import android.view.View
 import android.view.Window
 import android.view.WindowManager
-import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
 import androidx.core.widget.doAfterTextChanged
-import com.ladsers.passtable.android.databinding.DialogAskpasswordBinding
+import com.ladsers.passtable.android.databinding.DialogEnterdataBinding
+import java.util.*
+
 
 class MpRequester(
     private val context: Context,
@@ -29,11 +34,10 @@ class MpRequester(
     private val completeSavingWithNewPassword: (newPath: String, newPass: String) -> Boolean,
     private val afterSaving: (newPath: Uri) -> Unit
 ) {
-    private val binding: DialogAskpasswordBinding =
-        DialogAskpasswordBinding.inflate(window.layoutInflater)
-
     private var rememberMasterPass = false
     private var rememberingAvailable = true
+
+    private var passwordIsVisible = false
 
     fun startRequest(
         uri: Uri,
@@ -42,31 +46,43 @@ class MpRequester(
         canRememberPass: Boolean = true
     ) {
         val builder = AlertDialog.Builder(context)
-        val binding = DialogAskpasswordBinding.inflate(window.layoutInflater)
+        val binding = DialogEnterdataBinding.inflate(window.layoutInflater)
         builder.setView(binding.root)
-        binding.tvTitle.text = context.getString(R.string.dlg_title_enterMasterPassword)
 
         rememberMasterPass = false
         val biometricAuthAvailable = biometricAuth.checkAvailability()
         if (!canRememberPass) rememberingAvailable = false
         var closedViaButton = false
 
+        binding.tvTitle.text = if (isFileCreation) context.getString(R.string.dlg_ct_createNewFile)
+        else context.getString(R.string.dlg_ct_openTheFile)
+        binding.clPassword.visibility = View.VISIBLE
+
         binding.cbRememberPass.isChecked =
             ParamStorage.getBool(context, Param.CHECKBOX_REMEMBER_PASSWORD_BY_DEFAULT)
         binding.cbRememberPass.visibility =
             if (biometricAuthAvailable && rememberingAvailable) View.VISIBLE else View.GONE
-        if (showErrInvalidPass) binding.tvInvalidPassword.visibility = View.VISIBLE
 
-        val posBtnText = if (isFileCreation) context.getString(R.string.app_bt_save)
-        else context.getString(R.string.app_bt_ok)
-        builder.setPositiveButton(posBtnText) { _, _ ->
-            if (biometricAuthAvailable && rememberingAvailable) rememberMasterPass =
-                binding.cbRememberPass.isChecked
+        binding.btPositive.text = if (isFileCreation) context.getString(R.string.app_bt_save)
+        else context.getString(R.string.app_bt_enter)
+        binding.btPositive.icon =
+            if (isFileCreation) ContextCompat.getDrawable(context, R.drawable.ic_save)
+            else ContextCompat.getDrawable(context, R.drawable.ic_enter)
 
-            val pass = binding.etPassword.text.toString()
-            if (isFileCreation) completeCreation(pass) else completeOpening(pass)
-            closedViaButton = true
+        binding.btNegative.text = context.getString(R.string.app_bt_cancel).replaceFirstChar {
+            if (it.isLowerCase()) it.titlecase(
+                Locale.getDefault()
+            ) else it.toString()
         }
+        binding.btNegative.icon = ContextCompat.getDrawable(context, R.drawable.ic_close)
+
+        binding.btShowPass.setOnClickListener { showHidePassword(binding) }
+
+        if (showErrInvalidPass) {
+            binding.clErr.visibility = View.VISIBLE
+            binding.tvErrMsg.text = context.getString(R.string.dlg_ct_incorrectPassword)
+        }
+
         builder.setOnDismissListener {
             if (!closedViaButton) {
                 if (isFileCreation) {
@@ -80,31 +96,51 @@ class MpRequester(
         }
 
         builder.show().apply {
-            setupDialog(this)
-            val button = this.getButton(AlertDialog.BUTTON_POSITIVE)
-            button.isEnabled = false
-            binding.etPassword.doAfterTextChanged { x -> reactionToChanges(x, button) }
+            setupDialog(this, binding)
+
+            binding.btPositive.setOnClickListener {
+                if (biometricAuthAvailable && rememberingAvailable) rememberMasterPass =
+                    binding.cbRememberPass.isChecked
+
+                val pass = binding.etPassword.text.toString()
+                if (isFileCreation) completeCreation(pass) else completeOpening(pass)
+                closedViaButton = true
+                this.dismiss()
+            }
+
+            binding.btNegative.setOnClickListener {
+                this.dismiss()
+            }
         }
     }
 
     fun forNewFileRequest(pathNewFile: Uri) {
         val builder = AlertDialog.Builder(context)
-        val binding = DialogAskpasswordBinding.inflate(window.layoutInflater)
+        val binding = DialogEnterdataBinding.inflate(window.layoutInflater)
         builder.setView(binding.root)
-        binding.tvTitle.text = context.getString(R.string.dlg_title_enterNewMasterPassword)
-        binding.cbRememberPass.visibility = View.GONE
 
         var closedViaButton = false
 
-        builder.setPositiveButton(context.getString(R.string.app_bt_ok)) { _, _ ->
-            val pass = binding.etPassword.text.toString()
-            if (completeSavingWithNewPassword(pathNewFile.toString(), pass)) afterSaving(pathNewFile)
-            closedViaButton = true
+        binding.tvTitle.text = context.getString(R.string.dlg_ct_saveAs)
+        binding.clPassword.visibility = View.VISIBLE
+        binding.cbRememberPass.visibility = View.GONE
+
+        binding.btPositive.text = context.getString(R.string.app_bt_save)
+        binding.btPositive.icon = ContextCompat.getDrawable(context, R.drawable.ic_save)
+
+        binding.btNeutral.visibility = View.VISIBLE
+        binding.btNeutral.text = context.getString(R.string.app_bt_saveWithCurrent)
+        binding.btNeutral.icon = ContextCompat.getDrawable(context, R.drawable.ic_save)
+
+        binding.btNegative.text = context.getString(R.string.app_bt_cancel).replaceFirstChar {
+            if (it.isLowerCase()) it.titlecase(
+                Locale.getDefault()
+            ) else it.toString()
         }
-        builder.setNeutralButton(context.getString(R.string.app_bt_doNotChangePassword)) { _, _ ->
-            if (completeSaving(pathNewFile.toString())) afterSaving(pathNewFile)
-            closedViaButton = true
-        }
+        binding.btNegative.icon = ContextCompat.getDrawable(context, R.drawable.ic_close)
+
+        binding.btShowPass.setOnClickListener { showHidePassword(binding) }
+
         builder.setOnDismissListener {
             if (!closedViaButton) {
                 DocumentsContract.deleteDocument(contentResolver, pathNewFile)
@@ -115,25 +151,86 @@ class MpRequester(
         }
 
         builder.show().apply {
-            setupDialog(this)
-            val button = this.getButton(AlertDialog.BUTTON_POSITIVE)
-            button.isEnabled = false
-            binding.etPassword.doAfterTextChanged { x -> reactionToChanges(x, button) }
+            setupDialog(this, binding)
+
+            binding.btPositive.setOnClickListener {
+                val pass = binding.etPassword.text.toString()
+                if (completeSavingWithNewPassword(
+                        pathNewFile.toString(),
+                        pass
+                    )
+                ) afterSaving(pathNewFile)
+                closedViaButton = true
+                this.dismiss()
+            }
+
+            binding.btNeutral.setOnClickListener {
+                if (completeSaving(pathNewFile.toString())) afterSaving(pathNewFile)
+                closedViaButton = true
+                this.dismiss()
+            }
+
+            binding.btNegative.setOnClickListener {
+                this.dismiss()
+            }
         }
     }
 
     fun isNeedToRemember() = rememberMasterPass
 
-    private fun setupDialog(alertDialog: AlertDialog){
+    private fun setupDialog(
+        alertDialog: AlertDialog,
+        binding: DialogEnterdataBinding
+    ) {
         alertDialog.window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
         alertDialog.setCanceledOnTouchOutside(false)
         alertDialog.window!!.setSoftInputMode(
             WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE
         )
         binding.etPassword.requestFocus()
+        binding.etPassword.doAfterTextChanged { x ->
+            val isNotEmpty = x.toString().isNotEmpty()
+            if (!isNotEmpty) showHidePassword(binding, true) //hide password
+            binding.etPassword.typeface = ResourcesCompat.getFont(
+                context,
+                if (isNotEmpty) if (passwordIsVisible) R.font.overpassmono_semibold else R.font.passmono_asterisk
+                else R.font.manrope
+            )
+            binding.btShowPass.visibility = if (isNotEmpty) View.VISIBLE else View.INVISIBLE
+
+            val res = Verifier.verifyMp(x.toString())
+            binding.btPositive.isEnabled = res == 0
+            binding.clErr.visibility = if (res == 0) View.GONE else View.VISIBLE
+            val errMsg = when (res) {
+                1 -> context.getString(R.string.dlg_err_mpEmpty)
+                2 -> context.getString(R.string.dlg_err_mpInvalidChars) + ' ' + Verifier.getMpAllowedChars(
+                    context.getString(R.string.app_com_spaceChar)
+                )
+                3 -> context.getString(R.string.dlg_err_mpSlashChar)
+                else -> ""
+            }
+            binding.tvErrMsg.text = errMsg
+        }
     }
 
-    private fun reactionToChanges(text: Editable?, btnOk: Button){
-        btnOk.isEnabled = text.toString().isNotEmpty()
+    private fun showHidePassword(
+        binding: DialogEnterdataBinding,
+        isVisible: Boolean = passwordIsVisible
+    ) {
+        passwordIsVisible = !isVisible
+
+        binding.etPassword.transformationMethod =
+            if (passwordIsVisible) HideReturnsTransformationMethod.getInstance() else PasswordTransformationMethod.getInstance()
+        binding.etPassword.setSelection(binding.etPassword.text.length)
+
+        binding.etPassword.typeface = ResourcesCompat.getFont(
+            context,
+            if (passwordIsVisible) R.font.overpassmono_semibold else R.font.passmono_asterisk
+        )
+
+        binding.btShowPass.icon = ContextCompat.getDrawable(
+            context,
+            if (passwordIsVisible) R.drawable.ic_password_hide else R.drawable.ic_password_show
+        )
     }
 }
