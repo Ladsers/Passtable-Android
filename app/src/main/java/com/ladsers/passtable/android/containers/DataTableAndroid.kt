cc3ccacaa34e18e91ca88f22c48ed94edb75e342
Max Korolev <37272192+Ladsers@users.noreply.github.com>
2022-09-10 13:44:14 +0300

    Improved Google Drive write protection because it's unstable.

--- app/src/main/java/com/ladsers/passtable/android/containers/DataTableAndroid.kt
+++ app/src/main/java/com/ladsers/passtable/android/containers/DataTableAndroid.kt
@@ -14,9 +14,12 @@ class DataTableAndroid(
 
     override fun writeToFile(pathToFile: String, cryptData: String) {
         val uri = pathToFile.toUri()
-        //val outputStream = contentResolver.openOutputStream(uri, "rwt")
 
         //read-only for Google Drive files.
+        val gdrivePattern = "content://com.google.android.apps.docs.storage"
+        if (pathToFile.startsWith(gdrivePattern))
+            throw Exception("Writing to Google Drive is not possible. It's unstable.")
+
         val outputStream = contentResolver.openOutputStream(uri, "wt")
         val bufferedWriter = BufferedWriter(OutputStreamWriter(outputStream))
         bufferedWriter.use { out -> out.write(cryptData) }
